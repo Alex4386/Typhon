6d2ad3df07526def2f8ec8eaed35b9b3b27f5b05
Alex4386 <psh010209@gmail.com>
2023-02-13 13:44:41 +0900

    chore: streamlined codebase

--- src/main/java/me/alex4386/plugin/typhon/TyphonCache.java
+++ src/main/java/me/alex4386/plugin/typhon/TyphonCache.java
@@ -4,14 +4,21 @@ public class TyphonCache<T>  {
     T target;
     long lastUpdate = System.currentTimeMillis();
 
-    public static long cacheValidity = 500;
+    public long cacheValidity = 20;
 
     public TyphonCache(T target) {
         this.target = target;
     }
 
+    public TyphonCache(T target, long cacheValidity) {
+        this(target);
+        this.cacheValidity = cacheValidity;
+    }
+
     public boolean isExpired() {
-        return System.currentTimeMillis() - this.lastUpdate >= cacheValidity;
+        if (this.cacheValidity < 0) return true;
+        if (this.cacheValidity == 0) return false;
+        return System.currentTimeMillis() - this.lastUpdate >= this.cacheValidity;
     }
 
     public T getTarget() {
